e2a6e505f96f89a38026eafcb3628bd7d47e507d
xiyun <154321856@qq.com>
2018-10-17 22:43:17 +0800

    2018/10/17

--- Reading/js/header-footer.js
+++ Reading/js/header-footer.js
@@ -8,19 +8,15 @@ $(function(){
 
 $(function(){
 	var trigger = null;
-<<<<<<< HEAD
 	var hidertig = null;
 	$("#nav-hover .nav-li-first").hover(function(){
-=======
-	$("#nav-hover .nav-li-first,.classify-list").hover(function(){
->>>>>>> branch 'master' of https://github.com/TeamBestOne/ReadingStart.git
 		trigger =setTimeout(function(){
 			$("#nav-hover .classify-list").show();
 		},250);	
 	},function(){
 		clearTimeout(trigger);
 		$("#nav-hover .classify-list").hover(function(){
-			$("#nav-hover .classify-list").show();
+			$("#nav-hover .classify-list").show();	
 			clearTimeout(hidertig);
 		},function(){
 			$("#nav-hover .classify-list").hide();
