7031b095e10b95b8bafa960a6458453b9a0af07f
llplnb <781854733@qq.com>
2018-10-18 01:43:10 +0800

    林烈萍

--- Reading/js/starts.js
+++ Reading/js/starts.js
@@ -9,6 +9,18 @@ $(function(){
 	},function(){
 		$(this).css("border","solid 1px #ffb0b4");
 	})
+	//星星评价
+//	$(".star>img").hover(function(){
+//		$(this).attr("src","img/image1/star4.png")
+//	},function(){
+//		$(this).attr("src","img/image1/star2.png")
+//	})
+//	$(".star>img").each(function(index){
+//		$(this).hover(function(){
+//			alert(index)
+//		})
+//	})
+
 	//粉丝互动切换
 	$("#mon-ticket").click(function(){
 		$("#sty1").show();
@@ -35,22 +47,29 @@ $(function(){
 	})
 	
 	//选项卡切换
-	$("#content li:eq(0)").hover(function(){
+	$("#content li:eq(0)").click(function(){
 		$("#msg-tab1").show();
 		$("#msg-tab2").hide();
-//		$(#content li:eq(1).css("border-bottom","2px solid #ed4259");
 	})
-	$("#content li:eq(1)").hover(function(){
-//		$(this).css("border-bottom","2px solid #ed4259");
+	$("#content li:eq(1)").click(function(){
 		$("#msg-tab2").show();
 		$("#msg-tab1").hide();
 	})
      $("#content li").each(function(){
-		$(this).hover(function(){
-			$(this).css("border-bottom","solid 2px #ed4259");
-		},function(){
-			$(this).css("border-bottom","solid 2px white");
+		$(this).click(function(){
+			$(this).css("border-bottom","solid 2px #ed4259").siblings().css("border-bottom","solid 2px white");
 		})
 	})
 	
 })
+//文字上下滚动
+     function scrollDiv(){
+     	$("#fans-line").animate({marginTop:"-27px"},500,function(){
+     		$(this).css("margin-top","0").find("li:eq(0)").appendTo($(this));
+     		
+     	})
+     }
+     $(function(){  
+			setInterval('scrollDiv()',3000);
+			  
+		})
